f141e3f807521032f7eb486daae8428bf3eae8b6
Meda.Ioana <meda.ioanamaria@yahoo.com>
2021-05-06 01:22:51 +0300

    "CBYYD-62 Patient GUI"

--- src/main/java/cbyyd_app/controllers/patientGUIControllers.java
+++ src/main/java/cbyyd_app/controllers/patientGUIControllers.java
@@ -1,4 +1,23 @@
 package cbyyd_app.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class patientGUIControllers {
+    @FXML
+    public void DoctorScheduleHandler() {
+    }
+
+    @FXML
+    public void TreatementsTestsHandler() {
+    }
+
+    @FXML
+    public void AppointmentHandler() {
+    }
+
+    @FXML
+    public void ScheduleHandler() {
+    }
 }
