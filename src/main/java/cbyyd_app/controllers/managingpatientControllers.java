46869b46ca84af4f670aebca37e95b15fb5f22a2
Meda.Ioana <meda.ioanamaria@yahoo.com>
2021-05-07 00:29:41 +0300

    "CBYYD-59 GUI for add/delete patient from the list"

---
+++ src/main/java/cbyyd_app/controllers/managingpatientControllers.java
@@ -0,0 +1,18 @@
+package cbyyd_app.controllers;
+
+import javafx.fxml.FXML;
+
+public class managingpatientControllers {
+
+    @FXML
+    public void addHandler() {
+    }
+
+    @FXML
+    public void deleteHandler() {
+    }
+
+    @FXML
+    public void backHAndler() {
+    }
+}
