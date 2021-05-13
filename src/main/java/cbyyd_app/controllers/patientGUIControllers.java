8e9cda2b3d8fe15cce67a93179522fe254f21bf8
Meda.Ioana <meda.ioanamaria@yahoo.com>
2021-05-13 11:32:16 +0300

    "CBYYD-62 Logout button"

--- src/main/java/cbyyd_app/controllers/patientGUIControllers.java
+++ src/main/java/cbyyd_app/controllers/patientGUIControllers.java
@@ -1,25 +1,49 @@
 package cbyyd_app.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.text.Text;
 
 public class patientGUIControllers {
+
+    @FXML
+    Text seeTreatmentsMessage;
+
+    @FXML
+    Text seeDoctorScheduleMessage;
+
+    @FXML
+    Text logoutMessage;
+
     @FXML
     public void DoctorScheduleHandler() {
+        try {
+            seeDoctorScheduleMessage.getScene().setRoot(FXMLLoader.load(getClass().getResource("/seeDoctorSchedule.fxml")));
+        } catch (Exception e) {
+            seeDoctorScheduleMessage.setText(e.getMessage());
+        }
     }
 
     @FXML
     public void TreatementsTestsHandler() {
+
     }
 
     @FXML
     public void AppointmentHandler() {
+
     }
 
     @FXML
     public void ScheduleHandler() {
+
     }
 
-    @FXML
-    public void backHandler() {
+    public void logoutHandler() {
+        try {
+            logoutMessage.getScene().setRoot(FXMLLoader.load(getClass().getResource("/login.fxml")));
+        } catch (Exception e) {
+            logoutMessage.setText(e.getMessage());
+        }
     }
 }
