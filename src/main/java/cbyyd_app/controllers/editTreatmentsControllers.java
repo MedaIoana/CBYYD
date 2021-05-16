8bcc340f5ed6c4bc33f4d1dda915667838985a5f
Sefora-Iemima-Mezei <sefora.mezei@student.upt.ro>
2021-05-16 12:08:04 +0300

    "CBYYD-47 Edit Treatments"

--- src/main/java/cbyyd_app/controllers/editTreatmentsControllers.java
+++ src/main/java/cbyyd_app/controllers/editTreatmentsControllers.java
@@ -1,4 +1,35 @@
 package cbyyd_app.controllers;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.text.Text;
+
 public class editTreatmentsControllers {
+
+    @FXML
+    Text backMessage;
+
+    @FXML
+    Text editMessage;
+
+    @FXML
+    public void backHandler() {
+        try {
+            backMessage.getScene().setRoot(FXMLLoader.load(getClass().getResource("/manageTreatments.fxml")));
+        } catch (Exception e) {
+            backMessage.setText(e.getMessage());
+        }
+    }
+
+    @FXML
+    public void editHandler()
+    {
+        try {
+            editMessage.setText("The treatment was edited");
+        }
+        catch (Exception e)
+        {
+            editMessage.setText(e.getMessage());
+        }
+    }
 }
