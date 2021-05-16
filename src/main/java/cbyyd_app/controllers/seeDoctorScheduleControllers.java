f639577625e61a07bb95bc4d4bdd8943b0f17a7d
Meda.Ioana <meda.ioanamaria@yahoo.com>
2021-05-17 02:28:07 +0300

    "CBYYD-6 Showing the doctor schedule in patient interface"

--- src/main/java/cbyyd_app/controllers/seeDoctorScheduleControllers.java
+++ src/main/java/cbyyd_app/controllers/seeDoctorScheduleControllers.java
@@ -1,12 +1,38 @@
 package cbyyd_app.controllers;
 
+import cbyyd_app.services.UserService;
+import cbyyd_app.user.Week;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.text.Text;
 
-public class seeDoctorScheduleControllers {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class seeDoctorScheduleControllers implements Initializable {
+
     @FXML
     Text backMessage;
+
+    @FXML
+    private TableView<Week> tableView;
+    @FXML
+    private TableColumn<Week,String> hoursCollumn;
+    @FXML
+    private TableColumn<Week,String> MondayCollumn;
+    @FXML
+    private TableColumn<Week,String> TuesdayCollumn;
+    @FXML
+    private TableColumn<Week,String> WednesdayCollumn;
+    @FXML
+    private TableColumn<Week,String> ThursdayCollumn;
+    @FXML
+    private TableColumn<Week,String> FridayCollumn;
+
 
     public void backHandler() {
         try {
@@ -14,5 +40,16 @@ public class seeDoctorScheduleControllers {
         } catch (Exception e) {
             backMessage.setText(e.getMessage());
         }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        hoursCollumn.setCellValueFactory(new PropertyValueFactory<Week,String>("Hours"));
+        MondayCollumn.setCellValueFactory(new PropertyValueFactory<Week,String>("Monday"));
+        TuesdayCollumn.setCellValueFactory(new PropertyValueFactory<Week,String>("Tuesday"));
+        WednesdayCollumn.setCellValueFactory(new PropertyValueFactory<Week,String>("Wednesday"));
+        ThursdayCollumn.setCellValueFactory(new PropertyValueFactory<Week,String>("Thursday"));
+        FridayCollumn.setCellValueFactory(new PropertyValueFactory<Week,String>("Friday"));
+        tableView.getItems().addAll(UserService.seeSchedule(UserService.getMyDoctor(LoginRegistrationControllers.getUsernameP())));
     }
 }
